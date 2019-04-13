2719eb1c96c7da4898f969be76a9a371949d7874
dkekel <dkekel@gmail.com>
2019-04-13 19:28:12 +0200

    Localize login page
    Move text constants to localization file

--- src/main/java/ch/meyrin/lakeflower/oauthenticator/configuration/WebConfig.java
+++ src/main/java/ch/meyrin/lakeflower/oauthenticator/configuration/WebConfig.java
@@ -1,11 +1,14 @@
 package ch.meyrin.lakeflower.oauthenticator.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @EnableWebMvc
 @Configuration
@@ -21,5 +24,30 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    /**
+     * * @return default Locale set by the user
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    /**
+     * an interceptor bean that will switch to a new locale based on the value of the language parameter appended to a request:
+     *
+     * @param registry
+     * @language should be the name of the request param i.e  localhost:8010/api/get-greeting?language=fr
+     * <p>
+     * Note: All requests to the backend needing Internationalization should have the "language" request param
+     */
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
